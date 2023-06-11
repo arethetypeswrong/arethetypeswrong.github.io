744814f40013b5f5f17021b90c0c322c9549af36
Andrew Branch <andrew@wheream.io>
2023-06-11 12:09:33 -0700

    Add JSON viewer for details

--- packages/web/src/renderer.ts
+++ packages/web/src/renderer.ts
@@ -1,12 +1,14 @@
-import { subscribe, getState } from "./state";
+import "@andrewbranch/json-view/devtools.css";
+import JSONView from "@andrewbranch/json-view";
+import { subscribe, getState, type State } from "./state";
 import { updateView } from "./utils/updateView";
 import { CheckButton } from "./views/CheckButton";
 import { ChecksTable } from "./views/ChecksTable";
-import { Details } from "./views/Details";
 import { Message } from "./views/Message";
 import { ProblemList } from "./views/ProblemList";
 import corePackageJson from "@arethetypeswrong/core/package.json";
 import tsPackageJson from "typescript/package.json";
+import { Details } from "./views/Details";
 
 interface Events {
   onPackageNameInput: (value: string) => void;
@@ -27,6 +29,8 @@ export function subscribeRenderer(events: Events) {
     const webVersionElement = document.getElementById("web-version") as HTMLAnchorElement;
     const coreVersionElement = document.getElementById("core-version") as HTMLElement;
     const tsVersionElement = document.getElementById("ts-version") as HTMLElement;
+    const jsonView = new JSONView("Details", {});
+    detailsElement.appendChild(jsonView.dom);
 
     coreVersionElement.innerText = `v${corePackageJson.version}`;
     tsVersionElement.innerText = `v${tsPackageJson.version}`;
@@ -50,13 +54,19 @@ export function subscribeRenderer(events: Events) {
 
     subscribe(render);
 
-    function render() {
+    function render(prevState: State) {
       const state = getState();
       updateView(messageElement, Message, { isError: state.message?.isError, text: state.message?.text || "" });
       updateView(problemsElement, ProblemList, { analysis: state.analysis });
       updateView(resolutionsElement, ChecksTable, { analysis: state.analysis });
       updateView(checkButton, CheckButton, { disabled: !state.packageInfo.parsed });
       updateView(detailsElement, Details, { analysis: state.analysis });
+      if (state.analysis) {
+        jsonView.value = state.analysis;
+        if (!prevState.analysis) {
+          jsonView.collapse();
+        }
+      }
     }
   });
 }
