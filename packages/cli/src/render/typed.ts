9c6db02a531a797d2ce7b197bca94f82a50064e7
Andrew Branch <andrew@wheream.io>
2023-07-08 22:19:40 -0500

    Make --format auto for CLI

--- packages/cli/src/render/typed.ts
+++ packages/cli/src/render/typed.ts
@@ -7,13 +7,13 @@ import { marked } from "marked";
 import { filterProblems, problemAffectsEntrypoint, problemKindInfo } from "@arethetypeswrong/core/problems";
 import type { Opts } from "../index.js";
 import { moduleKinds, problemFlags, resolutionKinds } from "../problemUtils.js";
-import { tableFlipped } from "./tableFlipped.js";
+import { asciiTable } from "./asciiTable.js";
 import TerminalRenderer from "marked-terminal";
 
 export async function typed(analysis: core.Analysis, opts: Opts) {
   const problems = analysis.problems.filter((problem) => !opts.ignoreRules || !opts.ignoreRules.includes(problem.kind));
   const grouped = groupProblemsByKind(problems);
-  const subpaths = Object.keys(analysis.entrypoints);
+  const entrypoints = Object.keys(analysis.entrypoints);
   marked.setOptions({
     // @ts-expect-error the types are wrong (haha)
     renderer: new TerminalRenderer(),
@@ -43,86 +43,90 @@ export async function typed(analysis: core.Analysis, opts: Opts) {
     console.log(summaryTexts.join("") || defaultSummary);
   }
 
-  const entrypoints = subpaths.map((s) => {
+  const entrypointNames = entrypoints.map(
+    (s) => `"${s === "." ? analysis.packageName : `${analysis.packageName}/${s.substring(2)}`}"`
+  );
+  const entrypointHeaders = entrypoints.map((s, i) => {
     const hasProblems = problems.some((p) => problemAffectsEntrypoint(p, s, analysis));
     const color = hasProblems ? "redBright" : "greenBright";
-
-    if (s === ".") return chalk.bold[color](`"${analysis.packageName}"`);
-    else return chalk.bold[color](`"${analysis.packageName}/${s.substring(2)}"`);
+    return chalk.bold[color](entrypointNames[i]);
   });
 
-  if (opts.format === "table-flipped") {
-    const table = new Table({
-      head: ["", ...allResolutionKinds.map((kind) => chalk.reset(resolutionKinds[kind]))],
-      colWidths: [20, ...allResolutionKinds.map(() => 25)],
-    });
+  const getCellContents = memo((entrypoint: string, resolutionKind: core.ResolutionKind) => {
+    const problemsForCell = groupProblemsByKind(filterProblems(problems, analysis, { entrypoint, resolutionKind }));
+    const resolution = analysis.entrypoints[entrypoint].resolutions[resolutionKind].resolution;
+    const kinds = Object.keys(problemsForCell) as core.ProblemKind[];
+    if (kinds.length) {
+      return kinds
+        .map((kind) => (opts.emoji ? `${problemKindInfo[kind].emoji} ` : "") + problemKindInfo[kind].shortDescription)
+        .join("\n");
+    }
 
-    subpaths.forEach((subpath, i) => {
-      const point = entrypoints[i];
-      let row = [point];
+    const jsonResult = !opts.emoji ? "OK (JSON)" : "🟢 (JSON)";
+    const moduleResult = (!opts.emoji ? "OK " : "🟢 ") + moduleKinds[resolution?.moduleKind?.detectedKind || ""];
+    return resolution?.isJson ? jsonResult : moduleResult;
+  });
 
-      row = row.concat(
-        allResolutionKinds.map((kind) => {
-          const problemsForCell = groupProblemsByKind(
-            filterProblems(problems, analysis, { entrypoint: subpath, resolutionKind: kind })
-          );
-          const resolution = analysis.entrypoints[subpath].resolutions[kind].resolution;
-          const kinds = Object.keys(problemsForCell) as core.ProblemKind[];
-          if (kinds.length) {
-            return kinds
-              .map(
-                (kind) => (opts.emoji ? `${problemKindInfo[kind].emoji} ` : "") + problemKindInfo[kind].shortDescription
-              )
-              .join("\n");
-          }
-
-          const jsonResult = !opts.emoji ? "OK (JSON)" : "🟢 (JSON)";
-          const moduleResult = (!opts.emoji ? "OK " : "🟢 ") + moduleKinds[resolution?.moduleKind?.detectedKind || ""];
-          return resolution?.isJson ? jsonResult : moduleResult;
+  const flippedTable =
+    opts.format === "auto" || opts.format === "table-flipped"
+      ? new Table({
+          head: ["", ...allResolutionKinds.map((kind) => chalk.reset(resolutionKinds[kind]))],
         })
-      );
-
-      table.push(row);
+      : undefined;
+  if (flippedTable) {
+    entrypoints.forEach((subpath, i) => {
+      flippedTable.push([
+        entrypointHeaders[i],
+        ...allResolutionKinds.map((resolutionKind) => getCellContents(subpath, resolutionKind)),
+      ]);
     });
-    console.log(table.toString());
-    return;
   }
 
-  const table = new Table({
-    head: ["", ...entrypoints],
-    colWidths: [20, ...entrypoints.map(() => 35)],
-  }) as GenericTable<HorizontalTableRow>;
-
-  allResolutionKinds.forEach((kind) => {
-    let row = [resolutionKinds[kind]];
-
-    row = row.concat(
-      subpaths.map((subpath) => {
-        const problemsForCell = groupProblemsByKind(
-          filterProblems(problems, analysis, { entrypoint: subpath, resolutionKind: kind })
-        );
-        const resolution = analysis.entrypoints[subpath].resolutions[kind].resolution;
-        const kinds = Object.keys(problemsForCell) as core.ProblemKind[];
-        if (kinds.length) {
-          return kinds
-            .map(
-              (kind) => (opts.emoji ? `${problemKindInfo[kind].emoji} ` : "") + problemKindInfo[kind].shortDescription
-            )
-            .join("\n");
-        }
-
-        const jsonResult = !opts.emoji ? "OK (JSON)" : "🟢 (JSON)";
-        const moduleResult = (!opts.emoji ? "OK " : "🟢 ") + moduleKinds[resolution?.moduleKind?.detectedKind || ""];
-        return resolution?.isJson ? jsonResult : moduleResult;
-      })
-    );
-
-    table.push(row);
-  });
-
-  if (opts.format === "ascii") {
-    console.log(tableFlipped(table));
-  } else {
-    console.log(table.toString());
+  const table =
+    opts.format === "auto" || !flippedTable
+      ? (new Table({
+          head: ["", ...entrypointHeaders],
+        }) as GenericTable<HorizontalTableRow>)
+      : undefined;
+  if (table) {
+    allResolutionKinds.forEach((kind) => {
+      table.push([resolutionKinds[kind], ...entrypoints.map((entrypoint) => getCellContents(entrypoint, kind))]);
+    });
   }
+
+  switch (opts.format) {
+    case "table":
+      console.log(table!.toString());
+      break;
+    case "table-flipped":
+      console.log(flippedTable!.toString());
+      break;
+    case "ascii":
+      console.log(asciiTable(table!));
+      break;
+    case "auto":
+      const terminalWidth = process.stdout.columns || 133; // This looks like GitHub Actions' width
+      if (table!.width <= terminalWidth) {
+        console.log(table!.toString());
+      } else if (flippedTable!.width <= terminalWidth) {
+        console.log(flippedTable!.toString());
+      } else {
+        console.log(asciiTable(table!));
+      }
+      break;
+  }
+}
+
+function memo<Args extends (string | number)[], Result>(fn: (...args: Args) => Result): (...args: Args) => Result {
+  const cache = new Map();
+  return (...args) => {
+    const key = "" + args;
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
 }
