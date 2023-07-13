30a3a1d923ad24a58619204a3a1c12bae4f811f6
Hunter Heston <huntersamuelheston@gmail.com>
2023-07-12 20:03:53 -0700

    table working

--- packages/web/src/components/packageAnalysis.tsx
+++ packages/web/src/components/packageAnalysis.tsx
@@ -1,7 +1,8 @@
 // Top level component for rendering a package analysis result.
 
-import type { Analysis, Problem } from "@arethetypeswrong/core";
-import { problemKindInfo, problemsByKind } from "@arethetypeswrong/core/problems";
+import type { Analysis, Problem, ProblemKind, ResolutionKind } from "@arethetypeswrong/core";
+import { filterProblems, problemKindInfo, problemsByKind } from "@arethetypeswrong/core/problems";
+import { allResolutionKinds, groupProblemsByKind } from "@arethetypeswrong/core/utils";
 
 type PackageAnalysisProps = {
   analysis: Analysis;
@@ -10,60 +11,105 @@ type PackageAnalysisProps = {
 export default function PackageAnalysis({ analysis }: PackageAnalysisProps) {
   return (
     <div>
-      <h2>Package Analysis</h2>
-      <p>
+      <h2>
         {analysis.packageName} v{analysis.packageVersion}
-      </p>
+      </h2>
       <p>Package {analysis.types ? "includes" : "does not include"} types</p>
       <EntryPointTable analysis={analysis} />
-      {/* <ul>
-        {subPaths.map((subPath) => (
-          <li key={subPath}>{subPath}</li>
-        ))}
-      </ul> */}
     </div>
   );
 }
 
 function EntryPointTable({ analysis }: { analysis: Analysis }) {
-  const entrypoints = Object.keys(analysis.entrypoints);
-  const values = Object.values(analysis.problems);
-  console.log(analysis.problems);
-  console.log(values.sort((a, b) => (a.kind > b.kind ? 1 : -1)));
-
-  const problemMap = new Map<string, Problem[]>();
-
-  for (const problem of analysis.problems) {
-    if (!problemMap.has(problem.entrypoint)) {
-      problemMap.set(problem.entrypoint, []);
-    }
-    problemMap.get(problem.entrypoint)?.push(problem);
-  }
-  console.log(problemMap);
-
   return (
     <>
       <div>
         <UniqueProblemTypes problems={analysis.problems} />
+        <ProblemTable analysis={analysis} packageName={analysis.packageName} />
       </div>
     </>
   );
 }
 
+function ProblemTable({ analysis, packageName }: { analysis: Analysis; packageName: string }) {
+  const entrypoints = Object.keys(analysis.entrypoints);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          {entrypoints.map((entrypoint) => (
+            <th key={entrypoint}>{entrypoint.replace(".", packageName)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {allResolutionKinds.map((kind) => (
+          <tr>
+            <td>{kind}</td>
+            {entrypoints.map((entrypoint) => (
+              <TableData kind={kind} entrypoint={entrypoint} analysis={analysis} />
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function TableData({ kind, entrypoint, analysis }: { kind: ResolutionKind; entrypoint: string; analysis: Analysis }) {
+  const resolutionInfo = analysis.entrypoints[entrypoint].resolutions[kind];
+  const problemsForCell = Object.entries(
+    groupProblemsByKind(filterProblems(analysis, { resolutionKind: kind, entrypoint }))
+  );
+
+  console.log(problemsForCell);
+  return (
+    <td>
+      {problemsForCell.length
+        ? problemsForCell
+            .map(
+              ([kind, problem]) =>
+                problemKindInfo[kind as ProblemKind].emoji +
+                " " +
+                problemKindInfo[kind as ProblemKind].shortDescription +
+                (problem.length > 1 ? ` (${problem.length})` : "")
+            )
+            .join(" ")
+        : resolutionInfo.resolution?.isJson
+        ? "✅ (JSON)"
+        : "✅ " + moduleKinds[resolutionInfo.resolution?.moduleKind?.detectedKind || ""]}
+    </td>
+  );
+}
+
 function UniqueProblemTypes({ problems }: { problems: Problem[] }) {
   const byKind = problemsByKind(problems);
-  console.log(byKind);
 
   return (
     <ul>
-      {Object.keys(byKind).map((kind) => (
-        <li>
-          <p>
-            {problemKindInfo[kind as ProblemKind].emoji} {problemKindInfo[kind as ProblemKind].title}
-          </p>
-          <p>{problemKindInfo[kind as ProblemKind].description}</p>
-        </li>
-      ))}
+      {Object.keys(byKind).map((kind) => {
+        const info = problemKindInfo[kind as keyof typeof problemKindInfo];
+
+        return (
+          <li>
+            <p>{info.emoji} </p>
+            <a href={info.docsUrl}>{info.title}</a>
+            <p>{info.description}</p>
+          </li>
+        );
+      })}
     </ul>
   );
 }
+
+///////////////
+// helpers
+///////////////
+
+const moduleKinds = {
+  1: "(CJS)",
+  99: "(ESM)",
+  "": "",
+};
