cdf1b10f92f7108520588c42591eed392dd23114
Hunter Heston <huntersamuelheston@gmail.com>
2023-07-13 09:17:43 -0700

    Grabbing search params

--- packages/web/src/components/main.tsx
+++ packages/web/src/components/main.tsx
@@ -3,9 +3,14 @@ import type { ResultMessage } from "../../worker/worker";
 import PackageAnalysis from "./packageAnalysis";
 import UntypedPackage from "./untypedPackage";
 import PackageForm from "./packageForm";
+import { useSearchParams } from "react-router-dom";
 
 export default function Main() {
   const [checkResult, setCheckResult] = useState<ResultMessage | null>(null);
+  const [searchParams] = useSearchParams();
+
+  const packageName = searchParams.get("p");
+  console.log("Analyzing types for ", packageName);
 
   return (
     <main>
