c2a1e8621ccdc9048712357c00e4f83174634f0c
Hunter Heston <huntersamuelheston@gmail.com>
2023-07-14 08:30:04 -0700

    Render details using the JSONView, not quite working though.

--- packages/web/src/components/details.tsx
+++ packages/web/src/components/details.tsx
@@ -1,9 +1,17 @@
+import JSONView from "@andrewbranch/json-view";
 import type { Analysis } from "@arethetypeswrong/core";
+import { useEffect, useRef } from "react";
 
 export default function Details({ analysis }: { analysis: Analysis }) {
-  return (
-    <div id="details">
-      <pre>{JSON.stringify(analysis)}</pre>
-    </div>
-  );
+  const details = new JSONView("Details", { analysis: analysis });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.innerHTML = "";
+      ref.current.appendChild(details.dom);
+    }
+  }, [analysis]);
+
+  return <div ref={ref} id="details"></div>;
 }
