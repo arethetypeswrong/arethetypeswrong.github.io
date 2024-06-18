e051d9f23def2a5329d1de31b1aa8cbcdd10bb90
Marcel Laverdet <marcel@laverdet.com>
2024-06-18 14:59:48 -0500

    Remove top-level await

--- packages/core/src/internal/esm/cjsBindings.ts
+++ packages/core/src/internal/esm/cjsBindings.ts
@@ -1,7 +1,5 @@
 import type { Exports } from "cjs-module-lexer";
-import { init, parse as cjsParse } from "cjs-module-lexer";
-
-await init();
+import { parse as cjsParse } from "cjs-module-lexer";
 
 export function getCjsModuleBindings(sourceText: string): Exports {
   return cjsParse(sourceText);
